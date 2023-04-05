5d8cf41c354a8001dee41619ce378c2dc76836f5
Joanna Piper Morgan <morgjack@oregonstate.edu>
2023-04-05 15:09:54 -0700

    wip

--- spec_rad.py
+++ spec_rad.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 with np.load('spec_rad.npz') as data:
     spec_rad_si      = data['spec_rad_si']
@@ -9,6 +10,11 @@ with np.load('spec_rad.npz') as data:
     spec_rad_ratio   = data['spec_rad_ratio']
     N_ratio          = data['N_ratio']
 
+#fixing as spec rad function cant copmute on si when the reqired number
+# of itterations are so low
+for i in range(spec_rad_si.shape[0]):
+    spec_rad_si[i,0] = spec_rad_si[i,1]
+
 x = ratio
 y = mfp
 
@@ -16,29 +22,36 @@ xs = x.size
 ys = y.size
 [Xx, Yy] = np.meshgrid(y,x)
 
-fig,ax = plt.subplots(nrows=1, ncols=3, subplot_kw={"projection": "3d"})
+cmap = 'viridis'
+
+max_v = 3 #np.max(spec_rad_ratio)
+min_v = 0
+
+fig, ax = plt.subplots(nrows=1, ncols=3, subplot_kw={"projection": "3d"})
+#fig, ax = plt.subplots(nrows=1, ncols=3)
 
 #ax[0].plot_surface(Xx,Yy,spec_rad_oci, cmap='viridis')
-ax[0].imshow(spec_rad_oci, cmap='viridis', interpolation='nearest')
-
-ax[0].set_title('OCI SCB Spectral Radius Plot')
+ax[0].plot_surface(Xx, Yy, spec_rad_oci, cmap='viridis')
+#ax[0].set_title('OCI SCB Spectral Radius Plot')
 ax[0].set_xlabel(r'mfp [$\Sigma * \Delta x$]')
 ax[0].set_ylabel('Scattering Ratio [$Σ_s$/Σ]')
-ax[0].set_zlabel('Spectrial Radius [ρ]')
+ax[0].set_zlabel('Spectrial Radius OCI [ρ]')
 
 
 ax[1].plot_surface(Xx,Yy,spec_rad_si, cmap='viridis')
-ax[1].set_title('SI SCB Spectral Radius Plot')
+#ax[1].set_title('SI SCB Spectral Radius Plot')
 ax[1].set_xlabel(r'mfp [$\Sigma * \Delta x$]')
 ax[1].set_ylabel('Scattering Ratio [$Σ_s$/Σ]')
-ax[1].set_zlabel('Spectrial Radius [ρ]')
+ax[1].set_zlabel('Spectrial Radius SI [ρ]')
 
 ax[2].plot_surface(Xx,Yy,spec_rad_ratio, cmap='viridis')
-ax[2].set_title('ρ_si/ρ_oci')
+#ax[2].set_title('ρ_si/ρ_oci')
 ax[2].set_xlabel(r'mfp [$\Sigma * \Delta x$]')
-ax[2].set_ylabel(r'Scattering Ratio [$\sigma_s/\sigma$]')
-ax[2].set_zlabel(r'$\frac{N_{SI}}{N_{OCI}}$')
+ax[2].set_ylabel(r'Scattering Ratio [$\Sigma_s/\Sigma$]')
+ax[2].set_zlabel(r'Ratio of spectral radii $ρ_{SI}/ρ_{OCI}$')
 
+
+plt.tight_layout()
 
 plt.show()
 #plt.savefig('specrad_oci',dpi=600)
